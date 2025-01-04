a46ec5eb5c44f76f9022a9ea74faad3a8092f62c
TC <68698872+westoleaboat@users.noreply.github.com>
2025-01-04 21:21:12 +0200

    Update model-view logic
    Updated code logic for model-view structure

--- template.py
+++ template.py
@@ -1,53 +1,84 @@
-# PyQt GUI application template
-# qt_template.py : A blank application window
+import sys
+from os import path
 
-import sys                              # pass QApplication an actual list of script arguments
-from PyQt5 import QtWidgets as qtw      #
-from PyQt5 import QtGui as qtg          # main Qt modules.
-from PyQt5 import QtCore as qtc         #
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as qtc
 
 
-class MainWindow(qtw.QWidget):
+class Model(qtc.QObject):
+
+    error = qtc.pyqtSignal(str)
+
+    def save(self, filename, content):
+        print("save_called")
+        error = ''
+        if not filename:
+            error = 'Filename empty'
+        elif path.exists(filename):
+            error = f'Will not overwrite {filename}'
+        else:
+            try:
+                with open(filename, 'w') as fh:
+                    fh.write(content)
+            except Exception as e:
+                error = f'Cannot write file: {e}'
+        if error:
+            self.error.emit(error)
+
+
+class View(qtw.QWidget):
+
+    submitted = qtc.pyqtSignal(str, str)
 
     def __init__(self):
-        """
-        MainWindow constructor. Subclass of QWidget
+        super().__init__()
+        self.setLayout(qtw.QVBoxLayout())
+        self.filename = qtw.QLineEdit()
+        self.filecontent = qtw.QTextEdit()
+        self.savebutton = qtw.QPushButton(
+            'Save',
+            clicked=self.submit
+        )
+        self.layout().addWidget(self.filename)
+        self.layout().addWidget(self.filecontent)
+        self.layout().addWidget(self.savebutton)
+
+    def submit(self):
+        filename = self.filename.text()
+        filecontent = self.filecontent.toPlainText()
+        self.submitted.emit(filename, filecontent)
+
+    def show_error(self, error):
+        qtw.QMessageBox.critical(None, 'Error', error)
+
+
+class MainWindow(qtw.QMainWindow):
+
+    def __init__(self):
+        """MainWindow constructor.
 
         This widget will be our main window.
         We'll define all the UI components in here.
-
-        Constructor ends with a call to self.show(), 
-        so our MainWindow will take care of showing itself.
         """
         super().__init__()
         # Main UI code goes here
 
+        self.view = View()
+        self.setCentralWidget(self.view)
+
+        self.model = Model()
+
+        self.view.submitted.connect(self.model.save)
+        self.model.error.connect(self.view.show_error)
+
         # End main UI code
         self.show()
 
-# Main code execution
+
 if __name__ == '__main__':
-    '''
-    It's best practice to create the QApplication object at the 
-    global scope (outside of any function or class). 
-    This ensures that all Qt objects get properly closed 
-    and cleaned up when the application quits.
-
-    We pass sys.argv into QApplication(); Qt has several default
-    command-line arguments that can be used for debugging or to alter styles and themes.
-    These are processed by the QApplication constructor if you pass in sys.argv.
-
-    We're calling app.exec() inside a call to sys.exit; this is a small touch
-    that causes the exit code of app.exec() to be passed to sys.exit(), so we pass
-    appropriate exit codes to the OS, if the underlying Qt instance crashes for some reason.
-    '''
-    # create QApplication object
     app = qtw.QApplication(sys.argv)
-
     # it's required to save a reference to MainWindow.
     # if it goes out of scope, it will be destroyed.
-
-    # make MainWindow object
     mw = MainWindow()
-    # call QApplication.exec()
     sys.exit(app.exec())
